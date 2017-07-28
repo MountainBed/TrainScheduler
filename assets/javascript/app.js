31740e0d707f0bea7410f815e1edcedef1a253ad
Ross Blair <Ross Blair>
2017-07-28 19:44:08 -0400

    Initial HTML/CSS set up, JS needs to be created.

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyDhSiPM4j-ZFEtJ8UktDeMuB6BJwdTfYio",
+//     authDomain: "portkey-pass-scheduler.firebaseapp.com",
+//     databaseURL: "https://portkey-pass-scheduler.firebaseio.com",
+//     projectId: "portkey-pass-scheduler",
+//     storageBucket: "",
+//     messagingSenderId: "22210828612"
+//   };
+//   firebase.initializeApp(config);
